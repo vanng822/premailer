05b40b66b9bba8aec3b7da1b1d9a816932f971cb
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-02-18 00:26:23 +0100

    One more try to fix the tests for python26

--- premailer/tests/test_merge_style.py
+++ premailer/tests/test_merge_style.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import unittest
 import xml
-from nose.tools import assert_raises
+from nose.tools import raises
 from premailer.merge_style import csstext_to_pairs, merge_styles
 
 
@@ -14,9 +14,9 @@ class TestMergeStyle(unittest.TestCase):
         parsed_csstext = csstext_to_pairs(csstext)
         self.assertEqual(('font-size', '1px'), parsed_csstext[0])
         
+    @raises(xml.dom.SyntaxErr)
     def test_inline_invalid_syntax(self):
         # inline shouldn't have those as I understand
         # but keep the behaviour
         inline = '{color:pink} :hover{color:purple} :active{color:red}'
-        with assert_raises(xml.dom.SyntaxErr):
-            merge_styles(inline, [], [])
+        merge_styles(inline, [], [])
